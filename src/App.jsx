1419596f4bdc6226a849ec5d35f7648a2af4af0b
Nikhilkumarr-dev <nikhilkumarkarnakota@gmail.com>
2024-10-21 12:39:26 +0530

    added the cleanup function

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,33 @@
 import './App.css'
 import { useState,useEffect } from 'react';
 function App()
-{
-  const [currentTab,setCurrentTab]=useState(1)
-  const [tabData,setTabData]=useState({});
-  const [loading,setLoading]=useState(true);
-
-  useEffect(function(){
-    //sending a backend request to get data from this tab
-    // console.log("send request to backend to get data for the tab"+currentTab);
-    setLoading(true);
-    fetch("https://jsonplaceholder.typicode.com/todos/" + currentTab)
-    .then( async res=>{
-      const json =await res.json();
-      setTabData(json);
-      setLoading(false);
-    })
-       
-  },[currentTab])
-
-  //this is the dependency array this effect runs whenever the dependency changes 
-  
-  return <div>
-    <button  onClick={function()
-      {
-        setCurrentTab(1);
-      }
-    }  style={{color: currentTab==1 ? "red":'black'}}>1</button>
-    <button onClick={function()
-      {
-        setCurrentTab(2);
-      }
-    } 
-    style={{color:currentTab== 2 ? "red": "black"}}>2</button>
-    <button
-    onClick={function()
-      {
-        setCurrentTab(3);
-      }
-    }
-     style={{color:currentTab==3? "red":"black"}}>3</button>
-    <button
-     onClick={function()
-      {
-        setCurrentTab(4);
-      }
-    }
-     style={{color:currentTab==4? "red":"black"}}>4</button>
-
-     <br />
-
-     {loading ? "loading>>>" : tabData.title}
-     
+{ 
+  return 
+  <div>
+      <Timer />
   </div>
+}
+
+const Timer = () =>
+{
+  const [seconds,setSeconds]=useState(0);
+
+  useEffect(()=>{
+    let clock = setInterval(function(){
+      setSeconds(prev=>prev+1);
+    },1000)
+    
+
+    //cleanup function
+
+    return function()
+    {
+      clearInterval(clock);
+    }
+    //unsubscribing from the event and clearing it mers
+
+  },[])
+
+  return <div>{seconds}seconds elapsed</div>
 }
 export default App;
